c38357fbd057f6a1de66860c6d04aa74a3131a72
SrMelian <manu9898@gmail.com>
2018-06-19 11:52:40 +0100

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,22 @@
+let socket = io();
+socket.on('connect', function () {
+    console.log('Conectado al servidor');
+});
+
+// Escuchar
+socket.on('disconnect', function () {
+    console.log('Perdimos conexión con el servidor');
+});
+
+// Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Manuel',
+    mensaje: 'Hola Mundo',
+}, function (resp) {
+    console.log('respuesta server:', resp);
+});
+
+// Enviar información
+socket.on('enviarMensaje', function (mensaje) {
+    console.log('Servidor:', mensaje);
+});
